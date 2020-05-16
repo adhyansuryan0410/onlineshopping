9a0518f7e32e724f2b374ed320f4f594e07297ab
Adhyan Suryan <coolady56@gmail.com>
2020-05-16 10:41:34 +0530

    Add files via upload

---
+++ precache-manifest.e77e1bdb9dc4ee01d03e55e3477b323d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64fb6c3c1ed6f555918ee9a635ad936c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "851cf6bde32f9b23108b",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b99f72f42cd5fb942c39",
+    "url": "/static/css/main.41bdc59c.chunk.css"
+  },
+  {
+    "revision": "851cf6bde32f9b23108b",
+    "url": "/static/js/2.29c935d4.chunk.js"
+  },
+  {
+    "revision": "ed4400b9109211e81cc70e71e3c03c2c",
+    "url": "/static/js/2.29c935d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b99f72f42cd5fb942c39",
+    "url": "/static/js/main.cdde029e.chunk.js"
+  },
+  {
+    "revision": "d8e21e6f837d14ffe9b2",
+    "url": "/static/js/runtime-main.c8c94179.js"
+  }
+]);
